b620a2fbf130489e77d45434083014fd77364a7b
Viraj Chavan <virajchavan308@gmail.com>
2023-08-05 18:21:38 +0530

    cleaned code, adjusted typography, changed hero section layout

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,14 +6,12 @@ export default function Hero() {
     <div id="hero" className="hero">
       <div className="container">
         <div className="hero-content">
-          <div className="hero-text">
-            <h1 className="hero-heading">Welcome to AniWatch</h1>
-            <p className="hero-para">
-              A free anime streaming service, with subbed and dubbed anime and
-              resolution from standard to ultraHD
-            </p>
-            <Button children="Explore" buttonStyle="btn-primary" />
-          </div>
+          <h1 className="hero-heading">Welcome to AniWatch</h1>
+          <p className="hero-para">
+            A free anime streaming service, with subbed and dubbed anime and
+            resolution from standard to ultraHD.
+          </p>
+          <Button children="Explore" buttonStyle="btn-primary" />
         </div>
       </div>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,12 @@ export default function Navbar() {
     <nav className="navbar">
       <div className="container">
         <div className="nav-items">
-          <a className="logo">
+          <a className="nav-logo">
             <a href="#hero" className="logo-text">
               Aniwatch
             </a>
           </a>
-          <div className="nav-items-left">
+          <div className="nav-items-right">
             <a href="#features" className="nav-link">
               Features
             </a>
